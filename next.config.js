8b33deefbe531e9776c7bbeb9e0b4d90b1251895
sinsky <60662802+sinsky@users.noreply.github.com>
2022-10-28 02:23:57 +0900

    reconfig remote images

--- next.config.js
+++ next.config.js
@@ -5,6 +5,14 @@ const nextConfig = {
   experimental: {
     appDir: true,
   },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "via.placeholder.com",
+      }
+    ]
+  }
 };
 
 module.exports = nextConfig;
