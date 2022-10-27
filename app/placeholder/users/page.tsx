410dff1fbaf19f0e5ad207ee22bc6179ffa58373
sinsky <60662802+sinsky@users.noreply.github.com>
2022-10-28 01:26:24 +0900

    create users page

---
+++ app/placeholder/users/page.tsx
@@ -0,0 +1,35 @@
+import { use } from "react";
+import Link from "next/link";
+
+type User = {
+  id: number;
+  name: string;
+};
+
+const fetchUsers: () => Promise<User[]> = async () => {
+  const res = await fetch("https://jsonplaceholder.typicode.com/users");
+  return res.json();
+};
+
+const Page = () => {
+  const users = use(fetchUsers());
+  return (
+    <div>
+      <h1 className="my-4 text-4xl font-bold text-blue-700">JSON Placeholder Users!</h1>
+      <div className="m-4">
+        {users.map((user) => {
+          return (
+            <div key={user.id}>
+              {user.id}: {user.name}
+            </div>
+          );
+        })}
+      </div>
+      <div>
+        <Link href="/placeholder" className="bg-blue-200 hover:bg-blue-300 underline p-2">back to Placeholder List</Link>
+      </div>
+    </div>
+  );
+};
+
+export default Page;
